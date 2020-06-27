b18ef8d009d031f8c29e64d6bfd7da6f7051f414
Jessica Seo <jessicaseo83@gmail.com>
2020-06-27 23:02:55 +0000

    Add post_item page route to server.js file

--- server.js
+++ server.js
@@ -35,11 +35,14 @@ app.use(express.static("public"));
 // Note: Feel free to replace the example routes below with your own
 const widgetsRoutes = require("./routes/widgets");
 const usersRoutes = require("./routes/users");
+const postNewItems = require("./routes/postItem");
 
 // Mount all resource routes
 // Note: Feel free to replace the example routes below with your own
 app.use("/api/users", usersRoutes(db));
 app.use("/api/widgets", widgetsRoutes(db));
+app.use("/posts", postNewItems(db));
+
 // Note: mount other resources here, using the same pattern above
 
 // Home page
