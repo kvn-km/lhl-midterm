e98985e50de853f897a356827c8387b6e58b4770
Kevin Kim <kevinfkim@gmail.com>
2020-06-27 21:22:15 +0000

    testing routes before proceeding.

--- server.js
+++ server.js
@@ -38,8 +38,8 @@ const usersRoutes = require("./routes/users");
 
 // Mount all resource routes
 // Note: Feel free to replace the example routes below with your own
-app.use("/api/users", usersRoutes(db));
-app.use("/api/widgets", widgetsRoutes(db));
+app.use("/users", usersRoutes(db));
+app.use("/widgets", widgetsRoutes(db));
 // Note: mount other resources here, using the same pattern above
 
 // Home page
