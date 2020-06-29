2189c25498c446a577c8362c76d7ead50f2c5d2e
Jessica Seo <jessicaseo83@gmail.com>
2020-06-29 22:41:27 +0000

    Update GET request for search page

--- routes/search.js
+++ routes/search.js
@@ -8,17 +8,32 @@ const dbParams = require('../lib/db.js');
 const db = new Pool(dbParams);
 db.connect();
 
+const typeList = (db) => {
+  const type = {};
+  for(let item of db ) {
+    if (!type[item.type]) {
+      type[item.type] = 1
+    } else {
+      type[item.type]++;
+    }
+  }
+  return type;
+}
 
 module.exports = (db) => {
-  router.get("/seaarch/new", (req, res) => {
+  router.get("/new", (req, res) => {
 
     db.query(`SELECT * FROM items;`)
       .then(data => {
-
+        const items = data.rows;
+        const types = typeList(items);
+        const templateVar = { types: types }
+        res.render("search", templateVar)
       })
       .catch(e => {
         console.error(e);
         res.send(e)
       });
   });
+  return router;
 }
