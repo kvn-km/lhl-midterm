fae6d5a95f83f55c5fb59007cea485731ab732b7
Samy0412 <samantha.gadet@gmail.com>
2020-07-01 23:30:20 +0000

    add unfavourite endpoint + button

--- routes/user.js
+++ routes/user.js
@@ -17,8 +17,8 @@ module.exports = (db) => {
   const getUserFavouritesById = (userId) => {
     return db
       .query(
-        `SELECT * FROM items
-              JOIN favourites
+        `SELECT * FROM favourites
+              JOIN items
               ON favourites.item_id = items.id
               WHERE user_id = $1;`,
         [userId]
@@ -31,13 +31,11 @@ module.exports = (db) => {
   };
 
   // UNFAVOURITES an item
-  const unfavouriteItem = (itemId) => {
+  const unfavouriteItem = (itemId, userId) => {
     return db.query(
-      `DELETE FROM items
-       JOIN favourites
-       ON favourites.item_id = items.id
-       WHERE items.id= $1;`,
-      [itemId]
+      `DELETE FROM favourites
+       WHERE favourites.item_id = $1 AND favourites.user_id = $2;`,
+      [itemId, userId]
     );
   };
 
@@ -69,11 +67,32 @@ module.exports = (db) => {
     );
   };
 
+  // // MARKS the item as inactive
+  // const markItemAsInactive = (itemId) => {
+  //   return db.query(
+  //     `UPDATE items
+  //         SET is_active = FALSE
+  //           WHERE items.id= $1;`,
+  //     [itemId]
+  //   );
+  // };
+
+  // // MARKS the item as active
+  // const markItemAsActive = (itemId) => {
+  //   return db.query(
+  //     `UPDATE items
+  //       SET is_active = TRUE
+  //         WHERE items.id= $1;`,
+  //     [itemId]
+  //   );
+  // };
+
   //RENDERS the user page with its listings
   router.get("/", (req, res) => {
     const userId = req.session["user_id"];
     const username = req.session["username"];
     getUserFavouritesById(userId).then((favourites) => {
+      console.log("favourites:", favourites);
       getSellerItemsBySellerId(userId).then((sellerItems) => {
         let templateVars = { favourites, user: username, sellerItems };
         res.render("userpage", templateVars);
@@ -96,6 +115,31 @@ module.exports = (db) => {
     const itemId = req.params.id;
     markItemAsSold(itemId).then(() => res.redirect(`/user`));
   });
+
+  //UNFAVOURITE the item
+
+  router.post("/:id/favourite", (req, res) => {
+    const userId = req.session["user_id"];
+    const username = req.session["username"];
+    const itemId = req.params.id;
+    unfavouriteItem(itemId, userId).then(() => res.redirect(`/user`));
+  });
+
+  // //MARKS the item as inactive in the database
+  // router.post("/:id/inactive", (req, res) => {
+  //   const userId = req.session["user_id"];
+  //   const username = req.session["username"];
+  //   const itemId = req.params.id;
+  //   markItemAsSold(itemId).then(() => res.redirect(`/user`));
+  // });
+
+  // //MARKS the item as active in the database
+  // router.post("/:id/active", (req, res) => {
+  //   const userId = req.session["user_id"];
+  //   const username = req.session["username"];
+  //   const itemId = req.params.id;
+  //   markItemAsSold(itemId).then(() => res.redirect(`/user`));
+  // });
 
   return router;
 };
