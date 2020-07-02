154b6f79cc5a23c4dd1bf149f3af6243eeddd7bb
Kevin Kim <kevinfkim@gmail.com>
2020-07-02 16:59:23 +0000

    merge small change on master

--- routes/messages.js
+++ routes/messages.js
@@ -10,6 +10,8 @@ const bodyParser = require("body-parser");
 app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({ extended: true }));
 
+const help = require("../public/scripts/item-helper");
+
 module.exports = (db) => {
   router.get("/json/", (req, res) => {
     db.query(`SELECT * FROM messages;`)
@@ -20,11 +22,17 @@ module.exports = (db) => {
       .catch(error => { console.log("JSON GET Fail", error); });
   });
 
+
   router.get("/", (req, res) => {
+    console.log("user_id:", req.session.user_id);
+    console.log("username:", req.session.username);
     const cookies = { username: req.session.username, user_id: req.session.user_id };
-    let templateVars = {
-      user: cookies,
-    };
+    let templateVars = {};
+    help.createVarsSingle(db, cookies, req)
+      .then((data) => {
+        templateVars = data;
+        res.render("item", templateVars);
+      });
     res.render("messages", templateVars);
   });
 
